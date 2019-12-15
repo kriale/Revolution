6efe3256e6fe67ca64b3dfa0b6fa3e75aa25a25f
mmfrogovski <rogovskiy.vladislav@gmail.com>
2019-12-15 21:24:06 +0300

    chat with thrift

--- multiplayer/thrift/thrift-entities/src/main/resources/Enums.thrift
+++
@@ -1,7 +0,0 @@
-namespace java developer.remarks.thrift.v1.enums
-
-enum PersonState {
-    UNVERIFIED
-    VERIFIED
-    GOLD
-}
---
+++ multiplayer/thrift/thrift/chat.thrift
@@ -0,0 +1,35 @@
+namespace java com.angelomelonas.thriftwebchat
+
+service ChatService {
+    // Clients first subscribe to be able to send messages.
+    Message subscribe(1: SubscriptionRequest subscriptionRequest)
+
+    Message unsubscribe(1: UnsubscriptionRequest unsubscriptionRequest)
+
+    Message sendMessage(1: MessageRequest messageRequest);
+
+    list<Message> getMessages(MessagesRequest messagesRequest);
+}
+
+struct Message {
+    1: string message;
+    2: string username;
+    3: i64 timestamp;
+}
+
+struct MessageRequest {
+    1: string username;
+    2: string message;
+}
+
+struct SubscriptionRequest{
+    1: string username;
+}
+
+struct UnsubscriptionRequest{
+    1: string username;
+}
+
+struct MessagesRequest{
+    1: string username;
+}
